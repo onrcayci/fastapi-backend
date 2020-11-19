ad7fb65e589bbcb3e279a7b220478fa3c734c108
Onur Cayci <onur.cayci@mail.mcgill.ca>
2020-11-18 20:14:04 -0500

    Remove python typing

--- backend/simulation/music_synthesis.py
+++ backend/simulation/music_synthesis.py
@@ -1,34 +1,35 @@
 import numpy as np
 
-n: float = 0.0002
-p: float = 0.5
-G: float = 0.75
+n = 0.0002
+p = 0.5
+G = 0.75
 
-def music_synthesis(grid_size: int = 4, iterations: int = 3, start_point: list = [2, 2]):
-    u: np.array = np.zeros(shape=(grid_size, grid_size))
-    u1: np.array = np.zeros(shape=(grid_size, grid_size))
+def music_synthesis(grid_size = 4, iterations = 3, start_point = [2, 2]):
+    u = np.zeros(shape=(grid_size, grid_size))
+    u1 = np.zeros(shape=(grid_size, grid_size))
     u1[start_point[0]][start_point[1]] = 1
-    u2: np.array = np.zeros(shape=(grid_size, grid_size))
-    simulation_result: list = list()
+    u2 = np.zeros(shape=(grid_size, grid_size))
+    simulation_result = list()
     for i in range(iterations):
         u = calculate_inner_elements(u, u1, u2, grid_size)
         u = calculate_edge_elements(u, grid_size)
         u = calculate_corner_elements(u, grid_size)
         u2 = u1.copy()
         u1 = u.copy()
-        iteration_result: np.array = np.around(u, decimals=4)
+        iteration_result = np.around(u, decimals=4)
+        iteration_result = np.ndarray.tolist(iteration_result)
         simulation_result.append(iteration_result)
     return simulation_result
 
-def calculate_inner_elements(u: np.array, u1: np.array, u2: np.array, grid_size):
-    result: np.array = u.copy()
+def calculate_inner_elements(u, u1, u2, grid_size):
+    result = u.copy()
     for i in range(1, grid_size-1):
         for j in range(1, grid_size-1):
             result[i][j] = (p * (u1[i-1][j] + u1[i+1][j] + u1[i][j-1] + u1[i][j+1] - (4 * u1[i][j])) + (2 * u1[i][j]) - ((1 - n) * u2[i][j])) / (1 + n)
     return result
 
 def calculate_edge_elements(u, grid_size):
-    result: np.array = u.copy()
+    result = u.copy()
     for i in range(1, grid_size-1):
         result[0][i] = G * result[1][i]
         result[grid_size-1][i] = G * result[grid_size-2][i]
@@ -37,7 +38,7 @@ def calculate_edge_elements(u, grid_size):
     return result
 
 def calculate_corner_elements(u, grid_size):
-    result: np.array = u.copy()
+    result = u.copy()
     result[0][0] = G * result[1][0]
     result[grid_size-1][0] = G * result[grid_size-2][0]
     result[0][grid_size-1] = G * result[0][grid_size-2]
