a49d60daffb2bc33ae400001dffdb13017f112e9
Onur Cayci <onur.cayci@mail.mcgill.ca>
2020-11-21 22:40:27 -0500

    Serve static files from FastAPI backend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 from typing import List
 from numpy import ndarray
 from pydantic import BaseModel
@@ -7,6 +9,8 @@ from .simulation import music_synthesis
 
 app = FastAPI()
 
+app.mount("/app", StaticFiles(directory="public", html=True), name="public")
+
 class SimulationRequest(BaseModel):
     grid_size: int
     iterations: int
@@ -14,14 +18,18 @@ class SimulationRequest(BaseModel):
 
 @app.get("/")
 async def root():
+    return RedirectResponse(url="/app")
+
+@app.get("/api")
+async def api_root():
     return {"message": "Backend developed using FastAPI!"}
 
-@app.get("/simulate", response_model=List[List[List[float]]])
+@app.get("/api/simulate", response_model=List[List[List[float]]])
 async def get_music_simulation():
     result = music_synthesis()
     return result
 
-@app.post("/simulate", response_model=List[List[List[float]]])
+@app.post("/api/simulate", response_model=List[List[List[float]]])
 async def post_music_simulation(simulation_request: SimulationRequest):
     result = music_synthesis(
         grid_size=simulation_request.grid_size,
